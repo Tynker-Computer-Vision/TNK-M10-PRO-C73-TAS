a9c8a63d2f8d07aaac1a69a1a934fad6c028c98c
procodingclass <88050205+procodingclass@users.noreply.github.com>
2023-06-16 15:43:52 +0530

    Update main.py

--- TA3/main.py
+++ TA3/main.py
@@ -4,7 +4,7 @@ from keras.models import load_model
 
 model = load_model("keras_Model.h5", compile=False)
 
-class_names = open("labels.txt", "r").readlines()
+classNames = open("labels.txt", "r").readlines()
 
 camera = cv2.VideoCapture(0)
 
@@ -15,8 +15,8 @@ while True:
 
     # Print the class name, confidence score to screen output
     try:
-        image = cv2.putText(image, class_name[2:], (10, 30), cv2.FONT_HERSHEY_DUPLEX, 0.4, (125, 246, 55), 1)
-        image = cv2.putText(image, str(np.round(confidence_score * 100))[:-2], (10, 50), cv2.FONT_HERSHEY_DUPLEX, 0.4, (125, 246, 55), 1)
+        image = cv2.putText(image, className[2:], (10, 30), cv2.FONT_HERSHEY_DUPLEX, 0.4, (125, 246, 55), 1)
+        image = cv2.putText(image, str(np.round(confidenceScore * 100))[:-2], (10, 50), cv2.FONT_HERSHEY_DUPLEX, 0.4, (125, 246, 55), 1)
     except:
         pass
 
@@ -30,13 +30,13 @@ while True:
     prediction = model.predict(image)
     # Use np.argmax to find a index with highest value in prediction array
     index = np.argmax(prediction)
-    # Use the index to get the name from class_names
-    class_name = class_names[index]
-    # Get confidence_score from prediction[0] at index 
-    confidence_score = prediction[0][index]
+    # Use the index to get the className from classNames
+    className = classNames[index]
+    # Get confidenceScore from prediction[0] at index 
+    confidenceScore = prediction[0][index]
 
-    keyboard_input = cv2.waitKey(1)
-    if keyboard_input == 27:
+    keyboardInput = cv2.waitKey(1)
+    if keyboardInput == 27:
         break
 
 camera.release()
